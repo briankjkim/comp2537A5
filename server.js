12837b2334766958c2dc537c69a83927dfed610d
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-29 20:35:37 -0700

    restructure project for heroku

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 
 
@@ -14,3 +16,5 @@ app.listen(process.env.PORT || 5000, function (err) {
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 });
+
+app.use(express.static('./public'));
