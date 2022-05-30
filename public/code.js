439918ec77273b17d07bab9f2f016e4025d8fbb0
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-29 21:54:23 -0700

    copy Nabil's code because nothing works

--- public/code.js
+++ public/code.js
@@ -3,79 +3,41 @@ firstCard = undefined
 
 cardHasBeenFlipped = false
 
-
-pokemonGridImages = ''
-
-function processPokeResp(data) {
-    console.log(data.id)
-    pokemonGridImages += ` 
-      <div class="card">
-      <img id="${data.id}" class="front_face" src="${data.sprites.other["official-artwork"].front_default}">
-      <img id="${data.id}" class="back_face" src="back.jpg">
-      </div>`
-}
-
-
-async function loadPokemonImages() {
-    for (i = 1; i <= 4; i++) { 
-        // 1- generate a random number for Pokemon ID so that images can be loaded
-        pokemonRandomID = Math.floor(Math.random() * 33) + 1
-
-        // 2- initialize an AJAX request to my heroku api server
-        await $.ajax({
-            type: "GET",
-            url: `https://pokeapi.co/api/v2/pokemon/${pokemonRandomID}`,
-            success: processPokeResp
-        })
-
-        }
-        pokemonGridImages += `</div>`    
-    $("#game_grid").html(pokemonGridImages);
-    $("#game_grid").append(pokemonGridImages);
-}
-
-
-function setup(){
-    loadPokemonImages();
-
-    $("#game_grid").on("click", ".card", function() {
+function setup() {
+    $(".card").on("click", function () {
         $(this).toggleClass("flip")
 
-        if(!cardHasBeenFlipped){
-            // this is the first card
-            firstCard = $(this).find('.front_face')[0];
-            console.log(firstCard);
-            cardHasBeenFlipped = true;
-        }else{
-            // 2nd card
-            secondCard =  $(this).find('.front_face')[0];
+        if (!cardHasBeenFlipped) {
+            //captured first card
+            firstCard = $(this).find(".front_face")[0]
+            // console.log(firstCard);
+            cardHasBeenFlipped = true
+        } else {
+            secondCard = $(this).find(".front_face")[0]
             console.log(firstCard, secondCard);
             cardHasBeenFlipped = false;
 
             // check if you have match
-            if(
-                $(`#${firstCard.id}`).attr("src") 
-                == 
+            if (
+                $(`#${firstCard.id}`).attr("src")
+                ==
                 $(`#${secondCard.id}`).attr("src")
-                )
-            {
-                console.log("A Match!");
-                
-                $(`#${firstCard.id}`).parent().off("click");
-                $(`#${secondCard.id}`).parent().off("click");
-                // inc a global 
-                // disable cards
-            }else{
-                console.log("Not A Match!");
+            ) {
+                alert("A Match!");
+                $(`#${firstCard.id}`).parent().off("click")
+                $(`#${secondCard.id}`).parent().off("click")
+            } else {
+                alert("not a Match!");
                 setTimeout(() => {
                     $(`#${firstCard.id}`).parent().removeClass("flip")
                     $(`#${secondCard.id}`).parent().removeClass("flip")
                 }, 1000)
-                // unflip cards
+
             }
         }
     })
 }
+
 
 
 $(document).ready(setup)
--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const cors = require('cors');
 app.use(cors());
 
 
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.urlencoded({extends: true})); 
+
+
+// const mongoose = require('mongoose')  // mongoDB 
+// mongoose.connect(config.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}).then(() => console.log('MongoDB Connected...'))
+// .catch(err => console.log(err))
+
+
 
 // open the port for nodemon and heroku
 app.listen(process.env.PORT || 5000, function (err) {
@@ -16,5 +26,16 @@ app.listen(process.env.PORT || 5000, function (err) {
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html')
 });
+
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.use(express.static('./public'));
