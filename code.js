ad6d69b1ace9f46bcc2ff64f8aa6f9188a77e182
briankjkim <97319869+briankjkim@users.noreply.github.com>
2022-05-29 19:47:59 -0700

    base version of memory game

--- code.js
+++ code.js
@@ -4,8 +4,41 @@ hasFlippedCard = false;
 firstCard = undefined
 secondCard = undefined
 
+
+pokemonGridImages = ''
+
+function processPokeResp(data) {
+    console.log(data.id)
+    pokemonGridImages += ` 
+      <div class="card">
+      <img id="${data.id}" class="front_face" src="${data.sprites.other["official-artwork"].front_default}">
+      <img id="back${data.id}" class="back_face" src="back.jpg">
+      </div>`
+}
+
+
+async function loadPokemonImages() {
+    for (i = 1; i <= 9; i++) {
+        // 1- generate a random number for Pokemon ID so that images can be loaded
+        pokemonRandomID = Math.floor(Math.random() * 33) + 1
+
+        // 2- initialize an AJAX request to my heroku api server
+        await $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonRandomID}`,
+            success: processPokeResp
+        })
+
+        }
+        pokemonGridImages += `</div>`    
+    $("#game_grid").html(pokemonGridImages)
+}
+
+
 function setup(){
-    $(".card").on("click", function (){
+    loadPokemonImages();
+
+    $("#game_grid").on("click", ".card", function() {
         $(this).toggleClass("flip")
 
         if(!hasFlippedCard){
